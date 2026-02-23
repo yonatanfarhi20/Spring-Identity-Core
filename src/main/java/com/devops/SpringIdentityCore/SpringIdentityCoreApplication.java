654b35f8ffcd0679443fdee321c959e3749f9a8c
yonatanfarhi20 <yonatanfarhi1@gmail.com>
2026-02-23 22:36:37 +0200

    first commit

---
+++ src/main/java/com/devops/SpringIdentityCore/SpringIdentityCoreApplication.java
@@ -0,0 +1,13 @@
+package com.devops.SpringIdentityCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIdentityCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIdentityCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/SpringIdentityCore/SpringIdentityCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.SpringIdentityCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIdentityCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
